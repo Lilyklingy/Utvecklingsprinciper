b3e5945ea96ba1135dc9a3beb0ec76122e83b2d8
Lei Klingberg <zhang.lei92@outlook.com>
2021-04-07 16:50:53 +0200

    refactorin och readme fil

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 let cars = ['volvo', 'saab', 'bmw', 'tesla'];
+let fruits = ['apple', 'banana', 'orange'];
+let flowers = ['rose', 'tulip', 'lily', 'orchid'];
+
+//kod 1
+for (let i = 0; i < cars.length; i++){console.log(cars[i])}
+//kod 2
+let twoItems = cars.concat(fruits);
+for (let i = 0; i < twoItems.length; i++){
+    console.log(twoItems[i])
+}
+//kod 3
+function nameList (list){
+    for(let i = 0; i < list.length; i++){
+        console.log(list[i])
+    }
+}
+nameList(flowers)
